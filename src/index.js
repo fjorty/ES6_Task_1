a95d53880582aaa7e192dab7a8393ccb5ac7b71c
fjorty <naz.lavryk@gmail.com>
2019-03-08 23:17:55 +0200

    updated package.json

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import { Fighter } from "./fighter";
 import { ImprovedFighter } from "./improvedFighter";
 import { fight } from "./fight";
 
-// МС слева
-let leftFigter = new Fighter('Yarko9', 1, 10);
-
-// МС справа
-let rigthFighter = new ImprovedFighter('FjOrt', 10, 10000);
-
-// Пошумим бл*ть!!!
+let leftFigter = new Fighter('noob', 10, 50);
+let rigthFighter = new ImprovedFighter('pro', 20, 75);
 
 fight(leftFigter, rigthFighter, 25, 13, 45);
